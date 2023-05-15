48be1d31ead2dba07bf86cc4ca62f997d554c9cc
Rohit_Saxena_Jii <mrsaxena77@gmail.com>
2023-05-16 03:34:45 +0530

    third commit

--- script.js
+++ script.js
@@ -2,20 +2,23 @@ let input = document.querySelector(".Input");
 let submitButton = document.querySelector(".SubmitButton");
 let list = document.querySelector(".List");
 
-function tasksDisplayOnUi() {
+submitButton.addEventListener("click", function () {
+    let inputVal = input.value;
+    if (!inputVal || inputVal == " ") {
+        alert("Please Enter Your Desired Input!");
+        return;
+    }
 
-    submitButton.addEventListener("click", function() {
-        let inputVal = input.ariaValueMax;
-        if (!inputVal || inputVal == "") {
-            alert("Please Enter Your Desired Input!");
-            return;
-        }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputVal;
+        list.append(li);
 
-        else {
-            let li = document.createElement("li");
-            li.innerHTML = inputVal;
-            list.append(li);
+        li.addEventListener("dblclick", function (e) {
+            e.currentTarget.remove();
+        });
+    }
 
-        }
-    })
-}
+
+    input.value = "";
+});
