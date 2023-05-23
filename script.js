b0387cfa2a1447dfd80bf1c7c8570252852e7aff
Rohit_Saxena_Jii <mrsaxena77@gmail.com>
2023-05-24 00:31:55 +0530

    sixth comment

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ let submitButton = document.querySelector(".SubmitButton");
 let list = document.querySelector(".List");
 
 submitButton.addEventListener("click", function () {
+
     let inputVal = input.value;
     if (!inputVal || inputVal == " ") {
         alert("Please Enter Your Desired Input!");
         return;
     }
     else {
+        let span = document.createElement("span");
+        span.innerHTML = "x";
+        list.append(span);
         let li = document.createElement("li");
         li.innerHTML = inputVal;
         list.append(li);
-
-        let span = document.createElement("span");
-        span.innerHTML = "x";
-        li.append(span);
-
+        
         span.addEventListener("click", function(e) {
             e.currentTarget.parentElement.remove();
-        })
+        });
 
         // li.addEventListener("dblclick", function (e) {
         //     e.currentTarget.remove();
